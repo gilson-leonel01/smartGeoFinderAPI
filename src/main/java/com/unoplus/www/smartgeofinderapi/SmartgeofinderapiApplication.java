4077ac175a92d883c7292809fb8e4682c66d98a7
gilson-leonel01 <gilson.leonel05@gmail.com>
2025-09-30 20:37:30 +0100

    First commit

---
+++ src/main/java/com/unoplus/www/smartgeofinderapi/SmartgeofinderapiApplication.java
@@ -0,0 +1,13 @@
+package com.unoplus.www.smartgeofinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgeofinderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgeofinderapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unoplus/www/smartgeofinderapi/SmartgeofinderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unoplus.www.smartgeofinderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgeofinderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
